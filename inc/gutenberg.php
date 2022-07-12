932b752dad942a73f812fc597d5753572e800ff7
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-07-12 10:05:43 +0200

    added theme support for custom spacing and line heights

--- inc/gutenberg.php
+++ inc/gutenberg.php
@@ -13,6 +13,12 @@
  */
 function donovan_gutenberg_support() {
 
+	// Add theme support for dimension controls.
+	add_theme_support( 'custom-spacing' );
+
+	// Add theme support for custom line heights.
+	add_theme_support( 'custom-line-height' );
+
 	// Define block color palette.
 	$color_palette = apply_filters( 'donovan_color_palette', array(
 		'primary_color'    => '#ee1133',
